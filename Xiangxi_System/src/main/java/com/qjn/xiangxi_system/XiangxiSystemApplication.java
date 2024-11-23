8cc7f1a17d2d3579d7a1903f1484a21b494f0e8f
Qiu JiaNan <1822533929@qq.com>
2024-11-23 20:16:40 +0800

    后端初始文件

---
+++ Xiangxi_System/src/main/java/com/qjn/xiangxi_system/XiangxiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.qjn.xiangxi_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiangxiSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiangxiSystemApplication.class, args);
+    }
+
+}
---
+++ Xiangxi_System/src/test/java/com/qjn/xiangxi_system/XiangxiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qjn.xiangxi_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiangxiSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
