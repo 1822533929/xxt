00602f51d7beaad19852cdf7fe58e3770bd4f46e
Qiu JiaNan <1822533929@qq.com>
2024-11-23 21:44:04 +0800

    初始化项目结构

--- Xiangxi_System/src/main/java/com/qjn/xiangxi_system/XiangxiSystemApplication.java
+++ Xiangxi_System/src/main/java/com/qjn/xiangxi_system/XiangxiSystemApplication.java
@@ -1,9 +1,11 @@
 package com.qjn.xiangxi_system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.qjn.xiangxi_system.mapper")
 public class XiangxiSystemApplication {
 
     public static void main(String[] args) {
